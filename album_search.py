6cf331a1046ae5ed836ce657af339f3d1ad2c84e
davisc13 <davisc7@duq.edu>
2024-04-19 14:16:47 -0400

    This code defines a Flask route to handle GET requests for searching album names within a specified directory. The name of each subdirectory is treated as the title of the album. When a request is made to the /search_album endpoint with a query parameter album_name, the function search_album fetches this parameter, generates a search (case-insensitive) for album titles matching the subdirectory names in the 'Music' directory. The directory location is specified by SEARCH_FOLDER_PATH. The Flask route returns the search results as a JSON response.
    Using 'thefuzz' library and the 'process' module, it's functionality  provides the result of the search as an array of the 3 closest responses for the query. This array is then returned and encapsulated as jsonify object.
    
    Added checks for error from invalid directory or path

--- album_search.py
+++ album_search.py
@@ -12,11 +12,16 @@ app = Flask(__name__)
 # Helper func for creating a list to be searched for with fuzzy search
 def list_albums(music_directory):
     """
-    :param musicDirectory:
-    :return: list of album names from subdirectories names
+    :param music_directory is a specified path to a directory
+    :return: list of album names from subdirectories names or raise an error if DNE
 
     """
-    # list comprehension return
+    if not os.path.exists(music_directory):
+        raise ValueError(f"Directory -> {music_directory} DNE")
+    if not os.path.isdir(music_directory):
+        raise ValueError(f"Path -> {music_directory} is not a directory ")
+
+    # return with list comprehension
     return [name for name in os.listdir(music_directory)
             if os.path.isdir(os.path.join(music_directory, name))
             ]
@@ -24,14 +29,14 @@ def list_albums(music_directory):
 # Uses list from directory to build fuzzy search func
 def fuzzy_search_albums(query, music_dir):
     """
-    :param query: search entry
-    :param music_dir is the path to the directory holding subdir as albums
+    :param query is the search entry
+    :param music_dir is the path to the directory holding subdir names as album titles
     :return: match with query search and album in music directory list as well as
     an accuracy score of how well the response matches the query
     """
     albums = list_albums(music_dir)
     matches = process.extract(query, albums, limit=3)      # given some query, list of albums, and a constraint for the number of responses for the search
-    return matches
+    return matches # list of matches generated from fuzzywuzzy
 
 
 
