4459c8065ab3de9c7d25e521771583f674d49748
Claire <heimburgc1@duq.edu>
2024-04-23 18:38:27 -0400

    I used the json and os modules to return a JSON list of all tracks from the specified album. I used flask request to get the specified album name from the URL.

--- music_server.py
+++ music_server.py
@@ -1,18 +1,55 @@
-from flask import Flask
+from flask import Flask, request
 from waitress import serve
 
 import logging
+import json
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def server_home_page():
+	specific_album('Album 1')
 	return "This is the server's home page. Nothing interesting to see here."
 
 @app.errorhandler(Exception)
 def page_not_found(e):
 	print(e)
 	return '404 Not Found', 404
+
+@app.route('/specific_album')
+def specific_album():
+	# This will return a list of tracks from a specified album.
+
+	# Create a variable for the input of the specified album from the URL
+	album_name = request.args.get('album_name', default='*', type=str)
+	# URL: http://localhost:9999/specific_album?album_name=Album+1
+
+	# Initialize an empty dictionary to store the album and its tracks
+	specific_album = {}
+
+	# Relative path to the Music directory with Albums
+	directory_name = 'Music'
+
+	# Construct the path to the specific album
+	album_path = os.path.join(directory_name, album_name)
+
+	# Check if the directory specified by album_path exists
+	if os.path.isdir(album_path):
+		# Initialize an empty list of tracks
+		tracks = []
+		# List all tracks in the current album directory
+		for track in os.listdir(album_path):
+			if os.path.isfile(os.path.join(album_path, track)):
+				tracks.append(track)
+		# Add album and its tracks to album dictionary
+		specific_album[album_name] = tracks
+	else:
+		# The album does not exist
+		return "No such album exists."
+
+	# Return the list of tracks on the specified album
+	return json.dumps(specific_album)
 
 if __name__ == '__main__':
 	logging.getLogger('waitress').setLevel(logging.DEBUG)
