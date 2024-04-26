9d00a9e9d922e4e9ade8ba4ad77988fddf5d551a
davisc13 <davisc7@duq.edu>
2024-04-26 17:25:22 -0400

    Album names within a specified directory. The name of each subdirectory is treated as the title of the album. When a request is made to the /search_album endpoint with a query parameter album_name, the function search_album fetches this parameter, generates a search (case-insensitive) for album titles matching the subdirectory names in the 'Music' directory. The directory location is specified by SEARCH_FOLDER_PATH. The Flask route returns the search results as a JSON response.
    Using 'thefuzz' library and the 'process' module, it's functionality  provides the result of the search as an array of the 3 closest responses for the query. This array is then returned and encapsulated as jsonify object.
    
    Added checks for error from invalid directory or path

--- music_server.py
+++ music_server.py
@@ -1,9 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from waitress import serve
 
 import logging
 import json
 import os
+
+from album_search import *
 
 app = Flask(__name__)
 
@@ -51,6 +53,25 @@ def specific_album():
 
 	# Return the list of tracks on the specified album
 	return json.dumps(specific_album)
+
+
+SEARCH_FOLDER_PATH = r"C:\Users\davis_g7\OneDrive\Documents\Duq-CPMA-536-Spring-2024\Music"
+#SEARCH_FOLDER_PATH = r"Music"
+
+#http://localhost:9999/search_album?album_name=YourSearchQuery
+#http://localhost:9999/search_album?album_name=Album%201
+
+@app.route('/search_album', methods=['GET'])
+def search_album():
+    """
+    Builds query with Flask request object
+	match var is generated from source file album_search.py fuzzy_search_albums()
+    """
+    search = request.args.get('album_name', '').lower()
+    # Assuming fuzzy_search_albums is defined elsewhere and properly imported
+    match = fuzzy_search_albums(search, SEARCH_FOLDER_PATH)
+    return jsonify(match)
+
 
 if __name__ == '__main__':
 	logging.getLogger('waitress').setLevel(logging.DEBUG)
