b4ad08ff9de94b11f31ba3ab7c86337a2eda0c6e
Ahmad <166335713+csclass24@users.noreply.github.com>
2024-04-29 10:07:51 -0400

    Play select track from a select album.

--- music_server.py
+++ music_server.py
@@ -1,12 +1,10 @@
 
 from flask import Flask, send_from_directory, abort
-import os
 from waitress import serve
 import logging
 
 app = Flask(__name__)
-# Set the path to the music folder in the working directory
-app.config['MEDIA_FOLDER'] = os.path.join(os.getcwd(), 'Music')
+
 @app.route('/')
 def server_home_page():
 	return "This is the server's home page. Nothing interesting to see here."
@@ -15,13 +13,6 @@ def server_home_page():
 # :http://127.0.0.1:5000/play/Album%203/Track%204.mp3
 #For example: The above url will play track 4 from album 3.
 def play_song(album, track):
-    # Sanitize input to match directory and file naming conventions
-    valid_albums = ['Album 1', 'Album 2', 'Album 3']
-    album = ' '.join([word.capitalize() for word in album.split('_')])
-
-    if album not in valid_albums or not track.endswith('.mp3'):
-        return abort(404)  # Not found if invalid album or track
-
     try:
         # Use send_from_directory to serve the MP3 file
         return send_from_directory(f'Music/{album}', track)
