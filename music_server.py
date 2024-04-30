b9568278cff665ad1bba8628a7d600297dc1924e
davisc13 <davisc7@duq.edu>
2024-04-30 16:00:05 -0400

    Changed package for readinf duration of mp3 file from librosa to mutagen. Then created function to get the duration from a given path to audio file.

--- music_server.py
+++ music_server.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify
 from waitress import serve
-
 import logging
 import json
-import os
+from track_duration import *
 
 from album_search import *
 
@@ -63,14 +62,37 @@ SEARCH_FOLDER_PATH = r"C:\Users\davis_g7\OneDrive\Documents\Duq-CPMA-536-Spring-
 
 @app.route('/search_album', methods=['GET'])
 def search_album():
-    """
-    Builds query with Flask request object
-	match var is generated from source file album_search.py fuzzy_search_albums()
-    """
+    #"""
+    #Builds query with Flask request object
+	#match var is generated from source file album_search.py fuzzy_search_albums()
+    #"""
     search = request.args.get('album_name', '').lower()
     # Assuming fuzzy_search_albums is defined elsewhere and properly imported
     match = fuzzy_search_albums(search, SEARCH_FOLDER_PATH)
     return jsonify(match)
+
+
+@app.route('/get_track_length', methods=['GET'])
+def get_song_duration():
+    #"""
+    #Endpoint to retrieve the length of an audio track
+    #Retrieves the length of an audio track specified by song_path param
+    #Expects 'song_path' as a query parameter
+    #Uses librosa library to calculate the duration of the song
+    #"""
+
+	song_duration_search = request.args.get('song_path','')
+	#song_path = r"C:\Users\davis_g7\OneDrive\Documents\Duq-CPMA-536-Spring-2024\Music\Album 2\Track 2.mp3"
+	length = mutagen_length(song_duration_search)
+	ans_seconds = f"Track length in seconds : {length}"
+	#ans_minutes = f"Track length in minutes : {length/60}"
+	return ans_seconds
+
+# http://localhost:9999/get_track_length?song_path=C:\Users\davis_g7\OneDrive\Documents\Duq-CPMA-536-Spring-2024\Music\Album 2\Track 2.mp3
+
+
+
+
 
 
 if __name__ == '__main__':
