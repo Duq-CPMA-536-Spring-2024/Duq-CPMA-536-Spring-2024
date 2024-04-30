f1ed1d5c93c4444bc326836a3107ed3b64cfa8bd
Claire <heimburgc1@duq.edu>
2024-04-30 16:15:59 -0400

    Added a refactor function to return an album with its tracks. This function is used in a loop for issue1 to get all albums and their tracks. It is also used in issue2 to get the tracks for the specified album.

--- music_server.py
+++ music_server.py
@@ -7,90 +7,84 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def server_home_page():
-	return "This is the server's home page. Nothing interesting to see here."
+    return "This is the server's home page. Nothing interesting to see here."
+
 
 @app.errorhandler(Exception)
 def page_not_found(e):
-	print(e)
-	return '404 Not Found', 404
+    print(e)
+    return '404 Not Found', 404
+
 
 @app.route('/allalbums')
 def get_albums_with_tracks():
-	"""
-	Retrieve a complete list of albums and the contents
-	of each album on the server.
-	"""
+    """
+    Retrieve a complete list of albums and the contents
+    of each album on the server.
+    """
+    # Create an empty dictionary for all albums
+    all_albums = {}
 
-	# Initialize an empty dictionary to store albums and their tracks
-	albums={}
+    directory_name = 'Music'
 
-	# Relative path to the Music directory with Albums
-	directory_name = 'Music'
+    album_list = os.listdir(directory_name)
+    album_list.remove('.DS_Store')
 
-	# Check if the 'Music' directory exists
-	if not os.path.isdir(directory_name):
-		return "Music folder not found."
+    # Loop through the albums
+    for album in album_list:
+        specific_album = refactor(album)
+        all_albums[album] = specific_album
 
-	# Loop through the albums
-	for album in os.listdir(directory_name):
+    # Return the list of the albums and album contents on the server
+    return json.dumps(all_albums)
 
-		# Construct the path to the current album
-		album_path = os.path.join(directory_name, album)
-
-		# Check if the directory specified by album_path exists
-		if os.path.isdir(album_path):
-
-			# Initialize an empty list of tracks
-			tracks = []
-			# List all tracks in the current album directory
-			for track in os.listdir(album_path):
-				if os.path.isfile(os.path.join(album_path, track)):
-					tracks.append(track)
-
-			# Add album and its tracks to albums dictionary
-			albums[album] = tracks
-
-	# Return the list of the albums and album contents on the server
-	return json.dumps(albums)
 
 @app.route('/specific_album')
 def specific_album():
-	# This will return a list of tracks from a specified album.
+    # This will return a list of tracks from a specified album.
 
-	# Create a variable for the input of the specified album from the URL
-	album_name = request.args.get('album_name', default='*', type=str)
-	# URL: http://localhost:9999/specific_album?album_name=Album+1
+    # Create a variable for the input of the specified album from the URL
+    album_name = request.args.get('album_name', default='*', type=str)
+    # URL: http://localhost:9999/specific_album?album_name=Album+1
 
-	# Initialize an empty dictionary to store the album and its tracks
-	specific_album = {}
+    return refactor(album_name)
 
-	# Relative path to the Music directory with Albums
-	directory_name = 'Music'
+def refactor(name_of_album):
+    # This will allow for reuse of code between task1 and task2 as appropriate
 
-	# Construct the path to the specific album
-	album_path = os.path.join(directory_name, album_name)
+    # Initialize an empty dictionary to store the album and its tracks
+    specific_album = {}
 
-	# Check if the directory specified by album_path exists
-	if album_name == "":
-		return "No such album exists."
-	elif os.path.isdir(album_path):
-		# Initialize an empty list of tracks
-		tracks = []
-		# List all tracks in the current album directory
-		for track in os.listdir(album_path):
-			if os.path.isfile(os.path.join(album_path, track)):
-				tracks.append(track)
-		# Add album and its tracks to album dictionary
-		specific_album[album_name] = tracks
-	else:
-		# The album does not exist
-		return "No such album exists."
+    # Relative path to the music directory with albums
+    directory_name = 'Music'
 
-	# Return the list of tracks on the specified album
-	return json.dumps(specific_album)
+    # Check if the 'Music' directory exists
+    if not os.path.isdir(directory_name):
+        return "Music folder not found."
+
+    # Construct the path to the specific album
+    album_path = os.path.join(directory_name, name_of_album)
+
+    # Check if the directory specified by album_path exists
+    if name_of_album == "" or not os.path.isdir(album_path):
+        return "No such album exists."
+    else:
+        # Initialize an empty list of tracks
+        tracks = []
+        # List all tracks in the current album directory
+        for track in os.listdir(album_path):
+            if os.path.isfile(os.path.join(album_path, track)):
+                tracks.append(track)
+        # Add album and its tracks to album dictionary
+        specific_album[name_of_album] = tracks
+
+    # Return the list of tracks on the specified album
+    return json.dumps(specific_album)
+
 
 if __name__ == '__main__':
-	logging.getLogger('waitress').setLevel(logging.DEBUG)
-	serve(app, host='0.0.0.0', port=9999)
+    logging.getLogger('waitress').setLevel(logging.DEBUG)
+    serve(app, host='0.0.0.0', port=9999)
